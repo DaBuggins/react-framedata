c16d86c909544ccbbafc7aaa9d5bb3eed2d5b977
DaBuggins <willenrowe@gmail.com>
2023-06-28 22:05:20 +0100

    fixed character portrait links

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Character from "./Character";
 
 const characters = [
   {id: 1, name: "Ryu", image: "https://wiki.supercombo.gg/images/thumb/1/19/SF6_Ryu_Portrait.png/287px-SF6_Ryu_Portrait.png"},
-  {id: 2, name: "Chun_Li", image: "https://wiki.supercombo.gg/images/thumb/c/ca/SF6_Chun-Li_Portrait.png/250px-SF6_Chun-Li_Portrait.png"}
+  {id: 2, name: "Chun-Li", image: "https://wiki.supercombo.gg/images/thumb/c/ca/SF6_Chun-Li_Portrait.png/250px-SF6_Chun-Li_Portrait.png"},
+  {id: 3, name: "Ken", image: "https://wiki.supercombo.gg/images/thumb/0/05/SF6_Ken_Portrait.png/300px-SF6_Ken_Portrait.png"}
 ]
 
 function App() {
