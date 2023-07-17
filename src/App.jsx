712be7390fd4efaff7eaf63694991d766ac1303d
DaBuggins <willenrowe@gmail.com>
2023-07-17 06:31:24 +0100

    guile added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Character from "./Character";
 const characters = [
   {id: 1, name: "Ryu", image: "https://wiki.supercombo.gg/images/thumb/1/19/SF6_Ryu_Portrait.png/287px-SF6_Ryu_Portrait.png"},
   {id: 2, name: "Chun-Li", image: "https://wiki.supercombo.gg/images/thumb/c/ca/SF6_Chun-Li_Portrait.png/250px-SF6_Chun-Li_Portrait.png"},
-  {id: 3, name: "Ken", image: "https://wiki.supercombo.gg/images/thumb/0/05/SF6_Ken_Portrait.png/300px-SF6_Ken_Portrait.png"}
+  {id: 3, name: "Ken", image: "https://wiki.supercombo.gg/images/thumb/0/05/SF6_Ken_Portrait.png/300px-SF6_Ken_Portrait.png"},
+  {id: 4, name: "Guile", image: "https://wiki.supercombo.gg/images/thumb/e/ef/SF6_Guile_Portrait.png/295px-SF6_Guile_Portrait.png"}
 ]
 
 function App() {
---
+++ src/Guile.jsx
@@ -0,0 +1,12 @@
+export default function Guile() {
+    return (
+      <div>
+        <a href="https://wiki.supercombo.gg/w/Street_Fighter_6/Guile">
+          <img
+            src="https://wiki.supercombo.gg/images/thumb/e/ef/SF6_Guile_Portrait.png/295px-SF6_Guile_Portrait.png"
+            alt=""
+          />
+        </a>
+      </div>
+    );
+  }
