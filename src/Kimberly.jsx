6950b85925bf90819dedab27dd4ede7bf6bc3061
DaBuggins <willenrowe@gmail.com>
2023-07-18 06:28:53 +0100

    kimbo slice

---
+++ src/Kimberly.jsx
@@ -0,0 +1,15 @@
+
+export default function Kimberly() {
+    return (
+      <div>
+        <a href="https://wiki.supercombo.gg/w/Street_Fighter_6/Kimberly
+">
+          <img
+            src="https://wiki.supercombo.gg/images/thumb/7/73/SF6_Kimberly_Portrait.png/300px-SF6_Kimberly_Portrait.png"
+            alt=""
+          />
+        </a>
+      </div>
+    );
+  }
+  
